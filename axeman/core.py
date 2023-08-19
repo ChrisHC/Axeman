15afa7bd3d101bb5fb9dda6fb4bedb3fddbf0494
ChrisHC <chrishc@gmx.net>
2023-08-19 22:59:51 +0200

    only save domain data, skip all other data

--- axeman/core.py
+++ axeman/core.py
@@ -298,13 +298,13 @@ def process_worker(result_info):
             # header = "url, cert_index, chain_hash, cert_der, all_domains, not_before, not_after"
             lines.append(
                 ",".join([
-                    result_info['log_info']['url'],
-                    str(entry['cert_index']),
-                    chain_hash,
-                    cert_data['leaf_cert']['as_der'],
+                    #result_info['log_info']['url'],
+                    #str(entry['cert_index']),
+                    #chain_hash,
+                    #cert_data['leaf_cert']['as_der'],
                     '|'.join(cert_data['leaf_cert']['all_domains']),
-                    str(cert_data['leaf_cert']['not_before']),
-                    str(cert_data['leaf_cert']['not_after'])
+                    #str(cert_data['leaf_cert']['not_before']),
+                    #str(cert_data['leaf_cert']['not_after'])
                 ]) + "\n"
             )
 
